3b1559cad873e101f5de4d6985fe132f34a199c4
¨Denailly¨ <danilocalheirosjr@gmail.com>
2022-12-16 12:19:38 -0300

    Estudo de Arrays e Estruturas de Repetição em JavaScript

---
+++ Bootcamp_Inter/Javascript_Learning/06 - Arrays/aula.js
@@ -0,0 +1,8 @@
+//Crie um programa que dado um numero imprima sua tabuada
+
+const numTabuada = 9
+
+for (let i = 0; i <= 10; i++) {
+    let tabuada = numTabuada * i
+        console.log(tabuada)
+    }
---
+++ Bootcamp_Inter/Javascript_Learning/06 - Arrays/aula2.js
@@ -0,0 +1,10 @@
+// Crie um programa que seja capaz de percorrer uma lista de numeros e imprima cada numero PAR encontrado
+
+ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+for (let i = 0; i <= array.length; i++) {
+    if(array[i] % 2 === 0){
+        console.log(array[i])
+    }
+   }
